453e5e3a23f0db206dd21a7c0146669b30514641
Joris CHENNA joris64310@yahoo.fr <joris64310@yahoo.fr>
2023-08-17 08:56:53 +0200

    rails yeld mvp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Restaurant.destroy_all
+
+first_restaurant = { name: 'Tripletta', address: 'Cours du Médoc', phone_number: '05 59 54 70 00', category: 'italian' }
+second_restaurant = { name: 'Carioca', address: 'Gambetta', phone_number: '05 59 54 77 01', category: 'belgian' }
+third_restaurant = { name: 'madoname', address: 'chartrons', phone_number: '05 59 54 78 02', category: 'italian' }
+four_restaurant = { name: 'manpuku', address: 'place bourse', phone_number: '05 59 54 79 03', category: 'japanese' }
+five_restaurant = { name: 'le cromagnon', address: 'avenue hugo', phone_number: '05 59 54 80 04', category: 'french' }
+
+[first_restaurant, second_restaurant, third_restaurant, four_restaurant, five_restaurant].each do |attributes|
+  Restaurant.create!(attributes)
+end
